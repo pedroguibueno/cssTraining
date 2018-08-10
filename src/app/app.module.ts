8190c6d4fb21dbe61828129bac96a830000f8a60
root <root@localhost.localdomain>
2018-08-10 16:58:07 -0300

    save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,26 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { IndexComponent } from './index/index.component';
 import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { CompositionComponent } from './composition/composition.component';
+import { FeaturesComponent } from './features/features.component';
+import { TourComponent } from './tour/tour.component';
+import { ReviewComponent } from './review/review.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IndexComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutComponent,
+    CompositionComponent,
+    FeaturesComponent,
+    TourComponent,
+    ReviewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
